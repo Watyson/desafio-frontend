58d2a0c59d8f864f4cbcd6623d3e52c4628322f6
Watyson <pacht662@gmail.com>
2022-04-24 18:46:00 -0300

    Basis for creating schedules

---
+++ src/pages/schedules/index.js
@@ -0,0 +1,11 @@
+import Menu from "../../components/layout/menu.js"
+
+const Schedules = () => {
+    return (
+        <div>
+            <Menu/>
+            <h1>Tabela com os registros de agendamentos</h1>
+        </div>
+    )
+}
+export default Schedules
