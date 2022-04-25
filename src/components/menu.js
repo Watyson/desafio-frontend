781a5dc7659fdb3c3b0e5722651dc21942fcca4b
Watyson <pacht662@gmail.com>
2022-04-25 01:09:01 -0300

    Route to Home created

--- src/components/menu.js
+++ src/components/menu.js
@@ -3,6 +3,7 @@ import {Link} from "react-router-dom"
 const menu = () => {
     return (
         <ul>
+            <li><Link to="/">Home</Link></li>
             <li><Link to="/register">Registrar</Link></li>
             <li><Link to="/schedules">Agendamentos</Link></li>
         </ul>
